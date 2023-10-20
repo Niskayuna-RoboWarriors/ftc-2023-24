04fed4ff05bd4679a0bc463c3afc5e28158b8d0b
6460 team meber <6460@gmail.com>
2023-10-20 16:44:30 -0400

    Add compartment mechanisms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismDriving.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismDriving.java
@@ -8,7 +8,51 @@ import java.util.Map;
 /** Controls motors and servos that are not involved in moving the robot around the field.
  */
 public class MechanismDriving {
+    static final double COMPARTMENT_CLOSED_POS = 0;
+    static final double COMPARTMENT_OPEN_POS = 0.25;
 
+    /**
+     * Update left compartment
+     * @param robot
+     */
+    public void updateCompartmentLeft(Robot robot) {
+        robot.telemetry.addData("UPDATE COMPARTMENT LEFT SERVO STATE", robot.desiredCompartmentLeftState);
+        switch (robot.desiredCompartmentLeftState) {
+            case CLOSED:
+                robot.compartment_left.setPosition(COMPARTMENT_CLOSED_POS);
+                break;
+            case OPEN:
+                robot.compartment_left.setPosition(COMPARTMENT_OPEN_POS);
+                break;
+        }
+        robot.telemetry.addData("SET COMPARTMENT LEFT SERVO POSITION", robot.compartment_left.getPosition());
+    }
+
+    /**
+     * Update right compartment
+     * @param robot
+     */
+    public void updateCompartmentRight(Robot robot) {
+        robot.telemetry.addData("UPDATE COMPARTMENT RIGHT SERVO STATE", robot.desiredCompartmentRightState);
+        switch (robot.desiredCompartmentRightState) {
+            case CLOSED:
+                robot.compartment_right.setPosition(COMPARTMENT_CLOSED_POS);
+                break;
+            case OPEN:
+                robot.compartment_right.setPosition(COMPARTMENT_OPEN_POS);
+                break;
+        }
+        robot.telemetry.addData("SET COMPARTMENT RIGHT SERVO POSITION", robot.compartment_right.getPosition());
+    }
+
+    /**
+     * Update both compartments
+     * @param robot
+     */
+    public void updateCompartments(Robot robot) {
+        updateCompartmentRight(robot);
+        updateCompartmentLeft(robot);
+    }
 
 }
 
