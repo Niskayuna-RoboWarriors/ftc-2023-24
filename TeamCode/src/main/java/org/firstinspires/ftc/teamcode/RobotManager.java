46618ba7ab14c0cd752e6063ede9d916aaeb3ddd
6460 team meber <6460@gmail.com>
2023-10-20 18:47:09 -0400

    Add looping Tele-op RobotManager function outlines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManager.java
@@ -18,4 +18,98 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  */
 public class RobotManager {
 
+    // Controls dead zone of joystick and triggers so that actions don't accidently fire
+    static final double JOYSTICK_DEAD_ZONE_SIZE = 0.05;
+    static final double TRIGGER_DEAD_ZONE_SIZE = 0.05;
+
+    public enum AllianceColor {BLUE, RED};
+    public enum StartingSide {LEFT, RIGHT};
+    public enum ParkingPosition {LEFT, RIGHT, MIDDLE};
+    public Robot robot;
+    public AllianceColor allianceColor;
+    public StartingSide startingSide;
+
+    public MechanismDriving mechanismDriving;
+    public Navigation navigation;
+    public ComputerVision computerVision;
+
+    protected GamepadWrapper gamepads, previousStateGamepads;
+    public ElapsedTime elapsedTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+    /**
+     * makes robotmanager with subcomponents it's managing
+     * @param hardwareMap
+     * @param gamepad1
+     * @param gamepad2
+     * @param path
+     * @param allianceColor
+     * @param startingSide
+     * @param movementMode
+     * @param telemetry
+     * @param elapsedTime
+     */
+    public RobotManager(HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2,
+                        ArrayList<Position> path, AllianceColor allianceColor, StartingSide startingSide,
+                        Navigation.MovementMode movementMode, Telemetry telemetry, ElapsedTime elapsedTime) {
+
+        this.elapsedTime = elapsedTime;
+        this.allianceColor = allianceColor;
+        this.startingSide = startingSide;
+
+        elapsedTime.reset();
+        robot = new Robot(hardwareMap, telemetry, elapsedTime);
+        robot.telemetry.addData("auton path", path.size());
+        navigation = new Navigation(path, allianceColor, startingSide, movementMode);
+        mechanismDriving = new MechanismDriving();
+
+        computerVision = new ComputerVision(hardwareMap, robot.telemetry, elapsedTime);
+
+        gamepads = new GamepadWrapper(gamepad1, gamepad2);
+        previousStateGamepads = new GamepadWrapper();
+        previousStateGamepads.copyGamepads(gamepads);
+
+
+    }
+
+    /** Determine new robot desired states based on controller input (checks for button releases)
+     */
+    public void readControllerInputs() {
+    }
+
+    public void driveMechanisms(RobotManager robotManager) {
+        mechanismDriving.updateCompartments(robot);
+        mechanismDriving.updateSlides(robot);
+        mechanismDriving.updatePlaneSpring(robot);
+        mechanismDriving.updateFuzzyMotor(robot);
+
+    }
+    public void maneuver() {
+        navigation.updateStrafePower(hasMovementDirection(), gamepads, robot);
+
+        // Only move if one of the D-Pad buttons are pressed or the joystick is not centered.
+        boolean movedStraight = navigation.moveStraight(
+                gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_FORWARD),
+                gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_BACKWARD),
+                gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_LEFT),
+                gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_RIGHT),
+                robot
+        );
+        if (!movedStraight) {
+            navigation.maneuver(gamepads,
+                    gamepads.getAnalogValues(),
+                    robot);
+        }
+    }
+
+    public boolean hasMovementDirection() {
+        boolean dpadPressed = (gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_FORWARD)
+                || gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_BACKWARD)
+                || gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_LEFT)
+                || gamepads.getButtonState(GamepadWrapper.DriverAction.MOVE_STRAIGHT_RIGHT));
+        double stickDist = Math.sqrt(
+                Math.pow(gamepads.getAnalogValues().gamepad1LeftStickY,2)
+                        + Math.pow(gamepads.getAnalogValues().gamepad1LeftStickX,2));
+        boolean joystickMoved = stickDist >= RobotManager.JOYSTICK_DEAD_ZONE_SIZE;
+        return dpadPressed || joystickMoved;
+    }
 }
