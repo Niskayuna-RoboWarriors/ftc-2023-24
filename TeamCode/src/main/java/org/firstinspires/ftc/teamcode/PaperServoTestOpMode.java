01571581ddcbe2c2596687f44245d7e5dbe57e1e
6460 team meber <6460@gmail.com>
2023-12-14 15:45:26 -0500

    Made a new file for testing the drone launcher THIS DOES NOT NEED TO BE IN THE MAIN BRANCH

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PaperServoTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PaperServoTestOpMode.java
@@ -2,30 +2,27 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="MOTOR Test", group="Linear OpMode")
-public class MotorTestOpMode extends LinearOpMode {
+@TeleOp(name="SERVO Test", group="Linear OpMode")
+public class PaperServoTestOpMode extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private Servo clawServo = null;
-    private Servo rotationServo = null;
+    //private Servo clawServo = null;
+    //private Servo rotationServo = null;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        Servo servo = hardwareMap.get(Servo.class, "GOD DAMN PLANE SERVO");
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        DcMotor front_right  = hardwareMap.get(DcMotor.class, "front_right");
-        DcMotor front_left  = hardwareMap.get(DcMotor.class, "front_left");
-        DcMotor rear_right  = hardwareMap.get(DcMotor.class, "rear_right");
-        DcMotor rear_left  = hardwareMap.get(DcMotor.class, "rear_left");
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -33,14 +30,10 @@ public class MotorTestOpMode extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-//            if (runtime.milliseconds() % 2000 < 1000)
-//                motor.setPower(0.5);
-//            else
-//                motor.setPower(0.0);
-            rear_right.setPower(1.0);
-            rear_left.setPower(1.0);
-            front_right.setPower(1.0);
-            front_left.setPower(1.0);
+            if (runtime.milliseconds() % 2000 < 1000)
+                servo.setPosition(Servo.MIN_POSITION);
+            else
+                servo.setPosition(Servo.MAX_POSITION);
 
             // Show the elapsed game time.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
