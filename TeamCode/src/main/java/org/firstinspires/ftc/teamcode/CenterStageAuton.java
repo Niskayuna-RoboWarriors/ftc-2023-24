4577a8a1ada74a33c3dcdc619cf1da3c32e9916c
SuperTylerMan <ty.r.montgomery@gmail.com>
2024-01-06 21:10:14 -0500

    AUTON UPDATE
    AUTON UPDATE
    -Updated all the values for the shared preferences in autonomous
    -Added the shared preference configuration OpMode
    -Commented and documented more code.
    -Added a new file for the configured shared prefs
    
    -Tyler M, Stephen D.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageAuton.java
@@ -23,7 +23,7 @@ public class CenterStageAuton extends LinearOpMode {
     // Which Alliance we're on
     public enum AllianceColor {BLUE, RED};
     // Which side we're on (relative to our team)
-    public enum StartingSide {LEFT, RIGHT};
+    public enum StartingSide {FURTHER, CLOSER};
     // Where to park at the end of the autonomous period
     public enum ParkingPosition {LEFT, CENTER, RIGHT};
     // Movement mode during the autonomous period
@@ -73,7 +73,7 @@ public class CenterStageAuton extends LinearOpMode {
 
 
         /* Currently no waitTime functionality */
-
+        //make sure this is consistant with the values in ConfigureSharedPrefsOpMode
         if (allianceColorPref.equals("BLUE")) {
             allianceColor = AllianceColor.BLUE;
         }
@@ -81,11 +81,11 @@ public class CenterStageAuton extends LinearOpMode {
             allianceColor = AllianceColor.RED;
         }
 
-        if (startingSidePref.equals("LEFT")) {
-            startingSide = StartingSide.LEFT;
+        if (startingSidePref.equals("FURTHER")) { //If further from backdrop
+            startingSide = StartingSide.FURTHER;
         }
-        else if (startingSidePref.equals("RIGHT")) {
-            startingSide = StartingSide.RIGHT;
+        else if (startingSidePref.equals("CLOSER")) { //If closer to backdrop
+            startingSide = StartingSide.CLOSER;
         }
 
         if (movementModePref.equals("FORWARD_ONLY")) {
