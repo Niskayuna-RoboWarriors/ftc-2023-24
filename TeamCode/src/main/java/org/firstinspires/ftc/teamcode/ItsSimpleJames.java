bd0eeba555e404fd0509cadca86070b9b24b57a2
jSdCool <37940266+jSdCool@users.noreply.github.com>
2024-01-10 16:35:24 -0500

    auton to just park it is only 21 lines this took me less than 5 mins James

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ItsSimpleJames.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="ParkAuton", group="Linear OpMode")
+public class ItsSimpleJames extends LinearOpMode{
+    @Override
+    public void runOpMode() {
+        RobotManager robotManager = new RobotManager(hardwareMap, gamepad1, gamepad2, telemetry, new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS));
+        NavigationTeleOp.setDriveMotorPowers(Math.PI/2,1,0,robotManager.robot,false);
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        NavigationTeleOp.stopMovement(robotManager.robot);
+    }
+}
