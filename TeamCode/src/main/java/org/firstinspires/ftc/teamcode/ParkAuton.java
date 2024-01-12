a186a07c7784b25c95d33565ad1de62f9fa8e86d
6460 team meber <6460@gmail.com>
2024-01-12 18:57:05 -0500

    Implement waitMilliseconds for ParkAuton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkAuton.java
@@ -6,21 +6,32 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="ParkAuton", group="Linear OpMode")
 public class ParkAuton extends LinearOpMode{
+
+    private ElapsedTime elapsedTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
     @Override
     public void runOpMode() {
-        RobotManager robotManager = new RobotManager(hardwareMap, gamepad1, gamepad2, telemetry, new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS));
+        RobotManager robotManager = new RobotManager(hardwareMap, gamepad1, gamepad2, telemetry, elapsedTime);
         waitForStart();
         NavigationTeleOp.setDriveMotorPowers(0,1,0,robotManager.robot,false);
-        try {
-            Thread.sleep(200);
-        } catch (InterruptedException e){
-            throw new RuntimeException(e);
-        }
+//        try {
+//            Thread.sleep(200);
+//        } catch (InterruptedException e){
+//            throw new RuntimeException(e);
+//        }
+        waitMilliseconds(300);
         NavigationTeleOp.setDriveMotorPowers(Math.PI/2,1,0,robotManager.robot,false);
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            Thread.sleep(4000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        waitMilliseconds(4000);
         NavigationTeleOp.stopMovement(robotManager.robot);
-    }}
+    }
+
+    public void waitMilliseconds(long ms) {
+        double start_time = elapsedTime.time();
+        while (opModeIsActive() && elapsedTime.time() - start_time < ms) {}
+    }
+}
